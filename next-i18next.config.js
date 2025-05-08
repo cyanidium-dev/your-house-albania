382082b2f1369810d2e5d3712a3c6834132b2b78
Fedir <99473719+cyanidium1@users.noreply.github.com>
2025-05-08 03:20:24 +0200

    templated prj

---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  i18n: {
+    locales: ["en", "uk", "ru", "al"],
+    defaultLocale: "en",
+    localeDetection: true,
+  },
+  react: { useSuspense: false },
+};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import nextI18NextConfig from "./next-i18next.config"; // Импортируем настройки
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Для лучшей отладки и производительности
+  swcMinify: true, // Включаем minify для улучшения производительности
+  i18n: nextI18NextConfig.i18n, // Настройки i18n из next-i18next.config.ts
 };
 
 export default nextConfig;
