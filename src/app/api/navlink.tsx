2bc37e67ca2b5045983ed8cf31e1b578da1999c8
Fedir <99473719+cyanidium1@users.noreply.github.com>
2026-03-21 09:07:50 +0100

    Update navlink.tsx

--- src/app/api/navlink.tsx
+++ src/app/api/navlink.tsx
@@ -3,8 +3,13 @@ import { NavLinks } from '@/types/navlink'
 export const navLinks: NavLinks[] = [
   { key: 'home', href: '/' },
   { key: 'properties', href: '/properties' },
+<<<<<<< Updated upstream
   { key: 'favorites', href: '/favorites' },
   { key: 'blog', href: '/blog' },
+=======
+  { key: 'cities', href: '/cities' },
+  { key: 'blog', href: '/blogs' },
+>>>>>>> Stashed changes
   { key: 'contact', href: '/contactus' },
   { key: 'docs', href: '/documentation' },
 ]
